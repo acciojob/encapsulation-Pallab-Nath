d51e832903e7cf97b9b7fa782b8a4949c44f5509
pallab nath <you@example.com>
2023-03-14 10:19:33 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main (String args[])
+    {
+        RWOnly raw = new RWOnly("polu");
+        raw.setName("jaggy");
+        String s = raw.getName();
+        System.out.println(s);
+    }
 }
